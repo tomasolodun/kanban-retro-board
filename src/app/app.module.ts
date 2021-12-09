51d0e137c221549362dd9bfc53d4e282e1b59623
Tamara Solodun <tamara_solodun@epam.com>
2021-12-10 01:42:50 +0200

    fixed, repeating boards removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './shared/routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderModule } from './header/header.module';
+import { HeaderModule } from './board/header.module';
 import { BoardModule } from './board/board.module';
 
 // Firebase services + enviorment module
